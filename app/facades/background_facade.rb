4602c7ba4b444c2542f1677737cc140748319949
Matt Holmes <holmesm8@gmail.com>
2020-04-19 12:21:21 -0600

    New files to add

---
+++ app/facades/background_facade.rb
@@ -0,0 +1,10 @@
+class BackgroundFacade
+  attr_reader :id, :url
+
+  def initialize(location)
+    @id = nil
+    # background poro with unsplash json data
+    unsplash_data = BackgroundService.new(location)
+    @url = unsplash_data.background.url
+  end
+end
