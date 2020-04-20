6a0f655c32c7a7c2f29eaba29e365d6b65342b8f
Matt Holmes <holmesm8@gmail.com>
2020-04-20 16:16:11 -0600

    Add testing and update user controller to account for sad path

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates_uniqueness_of :email
-  validates_presence_of :password_digest
+  validates_uniqueness_of :email, presence: true
+  validates_presence_of :password_digest, require: true
+  validates_confirmation_of :password
   validates :api_key, uniqueness: true, presence: false
 
   has_secure_password
